43b508fa5fc8069796a8c614a7347a8d35e3c74b
sedx876 <52286148+sedx876@users.noreply.github.com>
2019-11-16 20:20:14 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,45 @@
+require './idea'
+class IdeaBoxApp < Sinatra::Base
+    set :method_override, true
+
+    not_found do
+        erb :error
+      end
+    
+      get '/' do
+        erb :index, locals: {ideas: Idea.all}
+      end
+
+      post '/' do
+      # 1. Create an idea based on the form parameters
+      idea = Idea.new(params['idea_title'], params['idea_description'])
+      # 2. Store it
+      idea.save
+      # 3. Send us back to the index page to see all ideas
+      redirect '/'
+        #params.inspect
+        # idea = Idea.new
+        # idea.save
+        # "Creating an IDEA!"
+      end
+
+      delete '/:id' do |id|
+        Idea.delete(id.to_i)
+        redirect '/'
+      end
+
+      get '/:id/edit' do |id|
+        idea = Idea.find(id.to_i)
+        erb :edit, locals: {id: id, idea: idea}
+      end
+
+      put '/:id' do |id|
+        data = {
+          :title => params['idea_title'],
+          :description => params['idea_description']
+        }
+        Idea.update(id.to_i, data)
+        redirect '/'
+      end
+
+end
